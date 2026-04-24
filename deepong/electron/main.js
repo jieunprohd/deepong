6ea724f65843f4be990291f55796ddbd5eac905e
jieunprohd <w2317@e-mirim.hs.kr>
2026-04-24 12:04:55 +0900

    feat: Electron 데스크톱 앱 통합
    Next.js 앱을 Electron 쉘에서 구동하기 위한 초기 세팅
    
    electron/main.js
    - BrowserWindow 생성 및 dev 모드에서 localhost:3000 로드
    - contextIsolation 활성화, nodeIntegration 비활성화
    - macOS activate/window-all-closed 라이프사이클 처리
    
    package.json
    - main 엔트리포인트를 electron/main.js로 지정
    - electron, electron:dev 스크립트 추가
    - electron, concurrently, wait-on, cross-env devDependencies 추가

---
+++ deepong/electron/main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const isDev = !app.isPackaged;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  if (isDev) {
+    win.loadURL("http://localhost:3000");
+    win.webContents.openDevTools();
+  } else {
+    win.loadFile(path.join(__dirname, "../out/index.html"));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
