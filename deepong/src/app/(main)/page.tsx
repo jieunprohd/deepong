921d78acb5657b6b0b76dd2d428d2e386bae63f8
baezy813 <w2316@e-mirim.hs.kr>
2026-04-25 16:06:53 +0900

    feat: add husky test message to HomePage component

--- deepong/src/app/(main)/page.tsx
+++ deepong/src/app/(main)/page.tsx
@@ -13,6 +13,9 @@ export default function HomePage() {
       <p className="mt-1 text-sm text-gray-400">
         대화를 시작하려면 친구를 추가하세요
       </p>
+      <p classNam="mt-1 text-sm text-gray-400">
+        husky test
+      </p>
     </div>
   );
 }
